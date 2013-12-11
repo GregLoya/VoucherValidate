55b6de5e8303e1dc5ee513e1721dda6f73a8c635
Greg <greg@Wakanada-Mac.local>
2013-12-11 09:41:12 -0500

    indextest

--- UserAdmin/WebFolder/index.waPage/scripts/index (1).js
+++ UserAdmin/WebFolder/index.waPage/scripts/index (1).js
@@ -2,10 +2,22 @@
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
+	var vbNewUser = {};	// @button
 	var login1 = {};	// @login
 // @endregion// @endlock
 
 // eventHandlers// @lock
+
+	vbNewUser.click = function vbNewUser_click (event)// @startlock
+	{// @endlock
+		//if(newUserName!='')
+		//{
+		//	newUser = ds.User.createUser(newUserName);
+		//	sources.user.addEntity(newUser);
+		//	sources.user.collectionRefresh(); 
+        //        // put the current entity in the datasource's entity collection
+        }
+	};// @lock
 
 	login1.logout = function login1_logout (event)// @startlock
 	{// @endlock
@@ -31,6 +43,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	
 
 // @region eventManager// @startlock
+	WAF.addListener("vbNewUser", "click", vbNewUser.click, "WAF");
 	WAF.addListener("login1", "logout", login1.logout, "WAF");
 	WAF.addListener("login1", "login", login1.login, "WAF");
 // @endregion
